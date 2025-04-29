fe891fc68dea7ed300d95c5ae3c42a6ee134ab45
KaFresh <aikawazunara@gmail.com>
2025-04-29 08:38:49 +0700

    Perbaiki port dan dependencies untuk Railway

--- app.py
+++ app.py
@@ -37,5 +37,7 @@ def home():
     
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Railway otomatis akan mengatur PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
